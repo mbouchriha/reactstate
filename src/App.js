daddcbd69bfcfe61c251320b5f0397173c1dd29b
mbouchriha <mbouchriha31@gmail.com>
2023-09-04 10:52:11 +0100

    state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      person: {
+        fullName: "mohamed amine BOUCHRIHA",
+        bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        imgSrc: "https://media.licdn.com/dms/image/C4D03AQGsX3EN5t_hsA/profile-displayphoto-shrink_800_800/0/1663919500021?e=2147483647&v=beta&t=EDdebE4UbSGzVQoACy1p0shVSz8E4Quu4oultlFiBjQ",
+        profession: "Software Developer",
+      },
+      show: false,
+      elapsedTime: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState((prevState) => ({
+        elapsedTime: prevState.elapsedTime + 1,
+      }));
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleProfile = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  render() {
+    const { person, show, elapsedTime } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Person Profile App</h1>
+        <button onClick={this.toggleProfile}>
+          {show ? "Hide Profile" : "Show Profile"}
+        </button>
+        {show && (
+          <div>
+            <h2>{person.fullName}</h2>
+            <p>{person.bio}</p>
+            <img src={person.imgSrc} alt={person.fullName} />
+            <p>Profession: {person.profession}</p>
+          </div>
+        )}
+        <p>Time Elapsed: {elapsedTime} seconds since mount.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
